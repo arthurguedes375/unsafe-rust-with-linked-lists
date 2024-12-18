b88f6820c65564f9b2b4bc4df08bb87f702a15ce
Arthur Guedes <arthurguedes375@gmail.com>
2024-12-18 14:36:20 -0300

    Fixing some typos

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ struct Node<T> {
     next: Option<Box<Node<T>>>,
 }
 
-impl<T> Node<T> {
+impl<T: Copy> Node<T> {
     pub fn get(&self, i: usize) -> Option<&Node<T>> {
         if i == 0 {
             return Some(self);
@@ -34,12 +34,12 @@ impl<T> Node<T> {
         None
     }
 
-    pub fn insert_at_the_end(&mut self, val: T) {
+    pub fn insert_at_the_end(&mut self, val: &T) {
         if let Some(next) = &mut self.next {
             (**next).insert_at_the_end(val);
         } else {
             let node = Box::new(Node {
-                value: val,
+                value: *val,
                 next: None,
             });
             self.next = Some(node);
@@ -49,13 +49,13 @@ impl<T> Node<T> {
     pub fn insert_in_place(&mut self, val: T) {
         /*
          * All of this code is made as a learning resource.
-         * Instead of all of this unsafe code you could just do: 'self.next.take()' (which would
+         * Instead of all of this unsafe code you could just do: 'self.next.take()' (which 
          * internally uses the mem::replace which uses the same code provided here.)
          */
         let next_val = unsafe {
             /*
              * Performs a bit by bit copy(bitwise copy) of the self.next.
-             * Since the self.next is just a Option with a box pointer it is not expansive.
+             * Since the self.next is just an Option with a box pointer it is not expansive.
              */
             let next_val = ptr::read(&self.next);
 
@@ -67,13 +67,13 @@ impl<T> Node<T> {
              */
             ptr::write(&mut self.next, None);
 
-            // Returns the box pointer...
+            // Returns the Option with the box pointer...
             next_val
         };
 
         let new_node = Box::new(Node {
             value: val,
-            // next: self.next.take(), // Instead of using all of the code above we can just call self.next.take() and it will work just fine, but since this is a learning project there you go...
+            // next: self.next.take(), // Instead of using all of the code above we can just call self.next.take() and it will work just fine, but since this is a learning project there you go... Don't forget tho, if you use self.next.take() COMMENT the code above
             next: next_val,
         });
         self.next = Some(new_node);
@@ -86,9 +86,9 @@ fn main() {
         next: None,
     };
 
-    first.insert_at_the_end(2);
-    first.insert_at_the_end(3);
-    first.insert_at_the_end(4);
+    first.insert_at_the_end(&2);
+    first.insert_at_the_end(&3);
+    first.insert_at_the_end(&4);
 
         /*
      * If we don't use the ManuallyDrop,
@@ -112,6 +112,7 @@ fn main() {
      * update the first[3] value, both the copyoffirst and the first values get updated lol. unsafe
      * rust is amazing.
      * Tho i am getting a seg fault for some reason.
+     * [ADDED LATER] Just Fixed seg fault lmao, read all of the comments to understand
      */
     first.get_mut(3).unwrap().value = 888888;
 
@@ -124,6 +125,7 @@ fn main() {
     // The problem is that, when rust tries to clean the first value(at the end of the program),
     // the operational system will notify that it is not owned by rust anymore and therefore it
     // will result in a seg fault.
+    // To fix the seg fault just comment the next line
     mem::drop(ManuallyDrop::into_inner(copyoffirst));
     // Or you can use the following:
     // mem::drop(&mut *copyoffirst);
