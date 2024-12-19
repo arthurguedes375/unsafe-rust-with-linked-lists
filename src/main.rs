f02d589466718dbc39ad539d2de299a4b9294d19
Arthur Guedes <arthurguedes375@gmail.com>
2024-12-19 11:52:51 -0300

    Trying to find the bug in ptr::drop_in_place

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::{
-    mem::{self, ManuallyDrop},
-    ptr,
+    io::{stdout, Write}, mem::{self, ManuallyDrop}, ptr, thread::sleep_ms
 };
 
 #[derive(Debug)]
@@ -9,7 +8,7 @@ struct Node<T> {
     next: Option<Box<Node<T>>>,
 }
 
-impl<'a, T: Copy> Node<T> {
+impl<T: Copy> Node<T> {
     pub fn get(&self, i: usize) -> Option<&Node<T>> {
         if i == 0 {
             return Some(self);
@@ -34,7 +33,7 @@ impl<'a, T: Copy> Node<T> {
         None
     }
 
-    pub fn insert_before(&'a mut self, val: &T) {
+    pub fn insert_before(&mut self, val: &T) {
         let current = mem::replace(self, Node {
             value: *val,
             next: None,
@@ -89,14 +88,22 @@ impl<'a, T: Copy> Node<T> {
 }
 
 fn main() {
-    let mut first = Node {
-        value: 1,
+    /*let mut first = ManuallyDrop::new(Node {
+        value: 1usize,
         next: None,
-    };
+    });
+    */
+    let mut first = ManuallyDrop::new(Node {
+        value: [2usize; 10_000_000],
+        next: None,
+    });
 
-    first.insert_at_the_end(&2);
+    println!("Got");
+
+    /*
+     first.insert_at_the_end(&2);
     first.insert_at_the_end(&3);
-    first.insert_at_the_end(&4);
+    first.insert_at_the_end(&4); */
 
     /*
      * If we don't use the ManuallyDrop,
@@ -112,12 +119,12 @@ fn main() {
     let copyoffirst = ManuallyDrop::new(unsafe { ptr::read(&mut first) });
     
     // This should not go to copyoffirst
-    first.insert_in_place(222222);
-    first.insert_before(&0);
+    /* first.insert_in_place(222222);
+    first.insert_before(&0); */
 
     // This proves that the 'inser_before' is not copying the data. Because both the copyoffirst
     // and the first get updated
-    first.get_mut(3).unwrap().value = 333333;
+/*     first.get_mut(3).unwrap().value = 333333; */
 
     /*
      * This proves that the insert_in_place is not copying the data in order to move it, when i
@@ -126,11 +133,69 @@ fn main() {
      * Tho i am getting a seg fault for some reason.
      * [ADDED LATER] Just Fixed seg fault lmao, read all of the comments to understand
      */
-    first.get_mut(4).unwrap().value = 44444;
+/*     first.get_mut(4).unwrap().value = 44444; */
 
-    let g = vec![first.get(0), copyoffirst.get(0)];
+    for i in 5..100_000 {
+        first.get_mut(i).unwrap().insert_in_place([i; 10_000_000]);
+    }
+    println!("Done creating");
 
-    println!("{:#?}", g);
+    /*
+     * with this commented code it is possible to create an infinite linked list because the fourth
+     * element points to the data that is in the first element (which eventually points to the
+     * fourth element). This happens because even tho we are copying the first data, the actual
+     * nodes are stored in the heap and then when we tell rust to write to the fourth element to
+     * point to the first it is actually pointing at the previous data
+     * Even if you don't print it or do anything with it eventually it may cause a stack overflow
+     * because eventually rust is going to try to drop and it seems like there is some type of
+     * recursion inside the drop implementation(i could not find the actualy code becase it seems
+     * like it is done under the hood by the rust compiler. Even the function ptr::drop_in_place
+     * gets replaced by the compiler code.)
+     * To fix this issue, you must use ManuallyDrop in the first, This way, you will never end up
+     * calling the Drop trait...
+     *
+     * Now apparently it is impossible to ask rust to free the actual heap linked list.
+     * Even if you call ManuallyDrop::drop(), it will run ptr::drop_in_place with the value of the
+     * list which has that problem of having the infinite linked list.
+     *
+     * But, you can just memory leak and pretend like that memory never existed(but it will still
+     * be there until you close the program). To do that you must call:
+     * ptr::drop_in_place(&mut first). Notice the difference.
+     * It is not the same as: ptr::drop_in_place(&mut *first); because the second one tries to drop
+     * the value of first. and the first one just drops the ManuallyDrop struct wrapper.
+     * [ADDED_LATER] It is funny because for some reason, calling manuallyDrop::drop or ptr::drop_in_place(&mut *first) doubles the memory used by the program even when there is no infinite linked list
+     *
+     * And by the way, if there is any nodes after the node index in get_mut, it will end up
+     * getting memory leaked because those nodes will be lost for ever but the memory won't get
+     * freed
+     */
+    /* unsafe {
+        ptr::write(&mut first.get_mut(49_000).unwrap().next, Some(Box::new(ptr::read(&mut *first))))
+    } */
+
+    // let g = vec![first.get(0), copyoffirst.get(0)];
+
+    // println!("{:?}", g);
+    // println!("{:?}", first);
+    println!("Created nasted loop and");
+
+    // It is funny because for some reason, calling manuallyDrop::drop or ptr::drop_in_place(&mut *first) doubles the memory used by the program even when there is no infinite linked list
+    /* unsafe {
+        ManuallyDrop::drop(&mut first);
+    } */
+
+    println!("Finished creating memory leak");
+
+    let mut i: usize = 0;
+    while i < 300{
+        print!("\rhey there {:10}   \r", &i);
+        i += 1;
+        stdout().flush().unwrap();
+        sleep_ms(50);
+    }
+
+    first.get_mut(100).unwrap().value = [3; 10_000_000];
+    println!("{:?}", first.get_mut(100).unwrap().value);
 
     // this will generate a seg fault because the data owned by copyoffirst is also owned by 'first'
     // then, it will get dropped and given back to the operational system.
@@ -138,7 +203,7 @@ fn main() {
     // the operational system will notify that it is not owned by rust anymore and therefore it
     // will result in a seg fault.
     // To fix the seg fault just comment the next line
-    mem::drop(ManuallyDrop::into_inner(copyoffirst));
+    // mem::drop(ManuallyDrop::into_inner(copyoffirst));
     // Or you can use the following:
     // mem::drop(&mut *copyoffirst);
 }
